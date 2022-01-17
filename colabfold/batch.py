58436abe61f9103b1176d662b97042869ab876fd
Jared Bard <jabard89@gmail.com>
2022-01-17 10:28:13 -0600

    uncommented problemattic stop at

--- colabfold/batch.py
+++ colabfold/batch.py
@@ -166,9 +166,6 @@ def predict_structure(
     if rank_by == "auto":
         # score complexes by ptmscore and sequences by plddt
         rank_by = "plddt" if not is_complex else "ptmscore"
-    logger.info(
-            f"Query {result_dir}, is_complex {is_complex}, {model_runner_and_params}"
-        )
     plddts, paes, ptmscore = [], [], []
     max_paes = []
     unrelaxed_pdb_lines = []
@@ -294,8 +291,8 @@ def predict_structure(
             # TODO: Those aren't actually used in batch
             relaxed_pdb_lines.append(relaxed_pdb_str)
         # early stop criteria fulfilled
-        #if mean_score > stop_at_score:
-        #    break
+        if mean_score > stop_at_score:
+           break
     # rerank models based on predicted lddt
     if rank_by == "ptmscore":
         model_rank = np.array(ptmscore).argsort()[::-1]
